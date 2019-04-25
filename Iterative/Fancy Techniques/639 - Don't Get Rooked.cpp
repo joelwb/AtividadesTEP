24300a437fdc8ab8041bbc74c68d816c7e16a16b
Joel Will Belmiro <20162BSI0511@cefetes.br>
2019-04-25 11:28:54 -0300

    Correção

--- Iterative/Fancy Techniques/639 - Don't Get Rooked.cpp
+++ Iterative/Fancy Techniques/639 - Don't Get Rooked.cpp
@@ -1,74 +1,63 @@
 #include <iostream>
+#include <vector>
 #include <string.h>
+#include <utility>
 
 using namespace std;
 
 int size_board;
 short board[4][4];
-short placed_rocks[4][4];
 short max_qtd;
-short qtd;
-short last_row_checked;
 
-bool can_place(int r, int c){
 
+bool can_place(int r, int c,  vector<pair<short,short>> impedidos, vector<pair<short,short>> markeds){
+    pair<short, short> ponto (r,c);
+    bool impedido = find(impedidos.begin(), impedidos.end(), ponto) != impedidos.end();
+    bool is_obstaculo = board[r][c];
+    bools is_marked = find(markeds.begin(), markeds.end(), ponto) != markeds.end();
+    return !(impedido || is_obstaculo || is_marked);
+}
+
+pair<short, short> next_ponto( vector<pair<short,short>> impedidos, vector<pair<short,short>> markeds){
+    for (int r = 0; r < size_board; r++){
+        for (int c = 0; c < size_board; c++){
+            if (can_place(r,c, impedidos, markeds)){
+                return make(r, c);
+            } 
+        }
+    }
+}
+
+vector<pair<short,short>> mark_impedidos(int r, int c,  vector<pair<short,short>> impedidos){
     for (int i = r-1; i >= 0; i--){
-        if (placed_rocks[i][c]) return false;
         if (board[i][c]) break;
+        impedidos.push_back(make_pair(i, c));
     }
     
     for (int i = r+1; i < size_board; i++){
-        if (placed_rocks[i][c]) return false;
         if (board[i][c]) break;
+        impedidos.push_back(make_pair(i, c));
     }
 
     for (int i = c-1; i >= 0; i--){
-        if (placed_rocks[r][i]) return false;
         if (board[r][i]) break;
+        impedidos.push_back(make_pair(r, i));
     }
 
-    return true;
+    for (int i = c+1; i < size_board; i++){
+        if (board[r][i]) break;
+        impedidos.push_back(make_pair(r, i));
+    }
+
+    return impedidos;
 }
 
-void backtrack(int r, int c){
-    if (r == size_board - 1){
-        for (int i = 0; i < size_board; i++){
-            if (board[r][i] == 0 && can_place(r, i)){
-                placed_rocks[r][i] = 1;
-                qtd++;
-            }
-        }
-        last_row_checked = r;
-    }else {
-        for (int i = c; i < size_board; i++){
-
-            if (board[r][i] == 0 && can_place(r, i)){
-                placed_rocks[r][i] = 1;
-                qtd++;
-                if (last_row_checked > r+1 && r < size_board - 1)
-                    backtrack(r+1,0);
-                
-            }
-        }
-        
-        if (last_row_checked > r+1 && r < size_board - 1)
-            backtrack(r+1,0);
-
-        last_row_checked = r;
-        if (qtd > max_qtd) max_qtd = qtd;
-        
-        if (c < size_board - 1){
-            qtd = 0;
-            for (int i = 0; i < r; i++){
-                for (int j = 0; j < size_board; j++) 
-                    if (placed_rocks[i][j]) qtd++;
-            }
-            
-            for (int i = r; i < size_board; i++) memset(placed_rocks[i], 0, 4*(sizeof(short)));
-            last_row_checked = r+1;
-            backtrack(r, c+1);
-        }
-    }
+void backtrack(int r, int c, vector<pair<short,short>> impedidos, vector<pair<short,short>> markeds){
+    
+    markeds.push(make_pair(r,c));
+    impedidos = mark_impedidos(r, c, impedidos);
+    pair<short, short> ponto = next_ponto(impedidos, markeds);
+    backtrack(ponto.first, ponto.second, impedidos, markeds);
 }
 
 
@@ -77,10 +66,8 @@ int main(){
 
     while (cin >> size_board && size_board){
         max_qtd = 0;
-        qtd = 0;
-        memset(placed_rocks, 0, 16*(sizeof(short)));
         memset(board, 0, 16*(sizeof(short)));
-        last_row_checked = 4;
+
         for (int i = 0; i < size_board; i++){
             cin >> row;
             for (int j = 0; j < size_board; j++){
@@ -88,7 +75,7 @@ int main(){
             }
         }
         
-        backtrack(0, 0);
+        backtrack(0, 0, {}, {});
         
         cout << max_qtd << endl;
     }
